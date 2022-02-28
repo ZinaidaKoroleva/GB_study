4ab3195b3cb4f1ac5f564316823e2c167a0c2420
Koroleva Zinaida <shymkova92@gmail.com>
2022-02-28 20:26:55 +0300

    Add HW1

---
+++ Homework/HW1/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1=Convert.ToInt32(Console.ReadLine());
+int number2=Convert.ToInt32(Console.ReadLine());
+if (number1 < number2)
+{
+Console.WriteLine("Меньшее число " + number1);
+Console.WriteLine("Большее число " + number2);
+}
+else
+{
+Console.WriteLine("Меньшее число " + number2);
+Console.WriteLine("Большее число " + number1);  
+}
