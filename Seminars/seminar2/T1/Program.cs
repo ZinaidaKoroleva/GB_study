0c8afcda081cb13fc1d04546cff445566fc3491d
Koroleva Zinaida <shymkova92@gmail.com>
2022-02-28 22:25:17 +0300

    Add Seminar tasks

---
+++ Seminars/seminar2/T1/Program.cs
@@ -0,0 +1,22 @@
+﻿Random rand = new Random();
+int number=68;
+for (int i=1; i<=10; i++)
+{
+    number=rand.Next(10,100);
+    Sravnenie(number);
+}
+void Sravnenie(int current_number)
+{
+ int t_place = current_number / 10;
+int d_place = current_number % 10;
+if (t_place>d_place)
+{
+    Console.WriteLine(current_number + " >> "+ t_place);
+}
+else
+{
+ Console.WriteLine(current_number + " >> "+ d_place);   
+}   
+}
+
+//Console.WriteLine(rand.Next(1,10));
---
+++ Seminars/seminar2/T2/Program.cs
@@ -0,0 +1,8 @@
+﻿string s = Console.ReadLine();
+while (s.Length !=3)
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число, введите заново");
+ s=Console.ReadLine();   
+}
+
+System.Console.WriteLine(s[1]);
