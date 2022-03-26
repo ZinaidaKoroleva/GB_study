7022ba6641ca2e8ec85ae4705c9621934ffb38aa
Koroleva Zinaida <shymkova92@gmail.com>
2022-03-26 23:49:34 +0300

    Add some changes

--- Seminars/seminar5/T35/Program.cs
+++ Seminars/seminar5/T35/Program.cs
@@ -1,4 +1,39 @@
 ﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
 //значения которых лежат в отрезке [10,99].
+int[] array = new int[123];
+FillArray();
+PrintArray();
+Number();
+Console.WriteLine("В отрезке содержится " + Number() + " чисел");
+void FillArray()
+{
+    Random rand = new Random();
+    int L = array.Length;
+    for (int i=0; i<L; i++)
+    {
+        array[i] = rand.Next(-10,201);
+    }
+}
+void PrintArray()
+{
+    int L = array.Length;
+    for (int i=0; i<L;i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine();
+}
 
-Console.WriteLine("Hello, World!");
+int Number ()
+{
+    int summ = 0;
+    int L = array.Length;
+    for(int i = 0; i < L; i++)
+    {   
+        if (array[i]<=99 && array[i]>=10)
+        {
+            summ = summ + 1;
+        }
+    }
+    return summ;
+} 
---
+++ Seminars/seminar5/T37/Program.cs
@@ -0,0 +1,36 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент
+// второй и предпоследний и т.д. Результат запишите в новом массиве.
+Random rand = new Random();
+int L = rand.Next(8,10);
+int[] array = new int[L];
+FillArray();
+PrintArray();
+multiplication();
+void FillArray()
+{
+    Console.Write("Задан массив: ");
+    Random rand = new Random();
+    for (int i=0; i<L; i++)
+    {
+        array[i] = rand.Next(0,11);
+    }
+}
+void PrintArray()
+{
+    for (int i=0; i<L;i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine();
+}
+
+void multiplication ()
+{
+    int MaxIndex = L-1;
+    Console.Write("Произведения пар чисел: ");
+    for(int i = 0; i < L / 2; i++)
+    {   
+       Console.Write(array[i] * array[MaxIndex - i ]+ " ");
+    }
+    
+} 
