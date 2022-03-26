7a589f5b48a464236fb1f8a8772c87086f567862
Koroleva Zinaida <shymkova92@gmail.com>
2022-03-26 22:28:45 +0300

    add lection 4

---
+++ Lections/lection2/lection6/T1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string [,] table = new string[2,5];
+
+table[1,2] = "слово";
+
+for (int rows = 0; rows < 24; rows++)
+{
+    for (int colums = 0; colums<5; colums++)
+    Console.WriteLine($"-{table[rows,colums]}-");
+}
---
+++ Lections/lection2/lection6/T2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Factoriaj(int n)
+{
+    if(n==1) return 1;
+    else return n * Factoriaj(n-1);
+}
+for (int i=1;i<40; i++)
+{
+ Console.WriteLine(Factoriaj(i));   
+}
