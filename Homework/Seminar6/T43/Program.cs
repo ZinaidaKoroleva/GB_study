7d66f288b3ee57717b39783664e94c0a1737aeae
Koroleva Zinaida <shymkova92@gmail.com>
2022-04-03 14:36:53 +0300

    Add HW 6

--- Homework/Seminar6/T43/Program.cs
+++ Homework/Seminar6/T43/Program.cs
@@ -1,15 +1,22 @@
 ﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
 //y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-double b1 = 1;
-double k1 = 0;
-double b2 = -1;
-double k2 = 2;
-if (k1==k2) Console.WriteLine("Прямые параллельны");
-else
-{
-    double x = (b2 - b1)/(k1-k2);
-    double y = k1*x + b1; // int y = k2*x + b2;
-    Console.WriteLine("Координата х: " + x);
-    Console.WriteLine("Координата y: " + y);
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+if ((k1 == k2)&&(b1 == b2)) Console.WriteLine("Прямые совпадают"); 
+else{
+    if(k1 == k2) Console.WriteLine("Прямые параллельны");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1; 
+        Console.WriteLine("Координата х: " + x);
+        Console.WriteLine("Координата y: " + y);
+    }
 }
