246d8585e6b396df47be23f7372cfcef11d02e00
Koroleva Zinaida <shymkova92@gmail.com>
2022-04-14 17:26:38 +0300

    Add seminar 9

--- Homework/Seminar9/T64/Program.cs
+++ Homework/Seminar9/T64/Program.cs
@@ -1,8 +1,22 @@
 ﻿// Задача 64: Задайте значения M и N. Напишите программу, 
 //которая выведет все натуральные числа кратные 3-ём в промежутке от M до N.
-int startNumber = 1;
-int finishNumber = 15;
-Recursion(startNumber,finishNumber);
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber<secondNumber)
+{
+   Recursion(firstNumber,secondNumber); 
+}
+else 
+{
+    int buff;
+    buff = firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = buff;
+    Recursion(firstNumber,secondNumber);
+}
+
 void Recursion(int n, int m)
 {
     if (n>m) return;
--- Homework/Seminar9/T66/Program.cs
+++ Homework/Seminar9/T66/Program.cs
@@ -1,14 +1,16 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу, 
 //которая найдёт сумму натуральных элементов в промежутке от M до N.
-int startNumber = 3;
-int finishNumber = 5;
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
 int summ = 0;
-Recursion(startNumber, finishNumber, summ);
+Recursion(firstNumber, secondNumber, summ);
 void Recursion(int n, int m, int s)
 {
     if (n > m)
     {
-        Console.WriteLine("Сумма элементов от " + startNumber + " до " + finishNumber + " равна: " + s);
+        Console.WriteLine("Сумма элементов от " + firstNumber + " до " + secondNumber + " равна: " + s);
         return;
     }
     s += n;
